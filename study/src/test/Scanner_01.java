d9fa14e019c0f31ddca2e177c595cffe6067b007
length <dlagudrb78@gmail.com>
2024-06-19 09:03:46 +0900

    Changes

--- study/src/test/Scanner_01.java
+++ study/src/test/Scanner_01.java
@@ -6,6 +6,6 @@ public class Scanner_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        //str ? 1 3항 연산자 쓸 거aaaa
+
     }
 }
